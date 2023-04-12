ee8c4d33786d510c93e9dda90b41b560b60cc732
Daniel Nascimento <danielsobralnascimento@hotmail.com>
2023-04-12 19:43:43 -0300

    Adicionando versões de PHP no docker do 8.2 ao 5.6 com SSL

---
+++ 5.6/public_html/update_php.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+php_default=$(php -v | cut -b5-7 | head -1)
+a2dismod 'php'$php_default
+sleep 3
+a2enmod php5.6
+sleep 3
+update-alternatives --set php /usr/bin/php5.6
+sleep 3
+service apache2 restart
---
+++ 7.4/public_html/update_php.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+php_default=$(php -v | cut -b5-7 | head -1)
+a2dismod 'php'$php_default
+sleep 3
+a2enmod php7.4
+sleep 3
+update-alternatives --set php /usr/bin/php7.4
+sleep 3
+service apache2 restart
